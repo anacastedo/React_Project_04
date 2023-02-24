eb63fa368b33f2506adf39e2631d2e6efe762173
Ana Castedo <anaccastedo@gmail.com>
2023-02-24 16:48:25 +0000

    React.memo prevents unecessary re-renders with some perform cost

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,4 +1,5 @@
 import MyParagraph from "./MyParagraph";
+import React from "react";
 
 const DemoOutput = props =>{
     console.log( 'running demo')
@@ -6,4 +7,4 @@ const DemoOutput = props =>{
 
 }
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
