f517e0b8fc4d60eac72dde3a118fd3ca8db69f62
Ana Castedo <anaccastedo@gmail.com>
2023-02-24 16:30:19 +0000

    When state is changed, component will re-render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
+import Button from "./components/UI/Button/Button";
 
 function App() {
+    const [showParagraph, setShowParagraph]=useState(false)
+
+    console.log('running')
+
+    const toggleParagraph = () =>{
+        setShowParagraph((prevShowParagraph)=>!prevShowParagraph)
+    }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+        {showParagraph && <p>This is new!</p>}
+        <Button onClick={toggleParagraph}>Toggle paragraph!</Button>
     </div>
   );
 }
